ad79390b71dc3cac3c78c188f34a1300abe59f2c
Tadeo Grach <tade.g.17@gmail.com>
2021-03-10 21:09:28 +0000

    fixing task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,9 +11,45 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	if (!tree->left && !tree->right)
+		return (1);
+	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
 		return (1);
 	return (0);
+}
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full.
+ * @tree: pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is full, else 0
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
+	return (0);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: balance
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
 
 /**
