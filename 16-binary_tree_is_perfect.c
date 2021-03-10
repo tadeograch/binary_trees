74c1df5b8c4e437162b83dce44634b6007ee32f6
Tadeo Grach <tade.g.17@gmail.com>
2021-03-10 12:52:17 +0000

    fixing task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -58,7 +58,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (!tree)
+	if (!tree->left && !tree->right)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
