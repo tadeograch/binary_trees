8fe78429fe7f700ef45fff486e47569df40561a7
Tadeo Grach <tade.g.17@gmail.com>
2021-03-10 12:45:20 +0000

    fixing task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,9 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if ((!node->left && !node->right) && node != NULL)
+	if (!node)
+		return (0);
+	if ((!node->left && !node->right))
 		return (1);
 	return (0);
 }
